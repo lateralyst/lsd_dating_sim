36d566f7f13b3b6ff8d689432eef32568f1f4448
nnoby <311017aaa@gmail.com>
2020-01-28 18:10:36 -0500

    ugh

--- scripts/Sprite3D2.gd
+++ scripts/Sprite3D2.gd
@@ -9,9 +9,14 @@ func _ready():
 	
 	pass # Replace with function body.
 func _process(delta):
-    var camera_pos = get_viewport().get_camera().global_transform.origin
-    camera_pos.y = 0
-    look_at(camera_pos, Vector3(0, 1, 0))
+	var camera_pos = get_viewport().get_camera().global_transform.origin
+	var camera_dir = -get_viewport().get_camera().global_transform.basis.z
+	camera_pos.y = 0
+	
+    
+	camera_pos += 10000 * camera_dir
+	
+	look_at(camera_pos, Vector3(0, 1, 0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
