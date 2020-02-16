0308fb538f8deb352a2d6eaf630d78790691853d
nnoby <311017aaa@gmail.com>
2020-02-16 17:14:53 -0500

    ed2

--- artfarmculling4.gd
+++ artfarmculling4.gd
@@ -1,13 +1,13 @@
 extends Spatial
 
-
-var mvmt = 0
+var base_translation = Vector3(0,0,0)
+var mvmt = 4000
 
 func _ready():
 	pass
 	
 func _process(delta):
-#	var speed = delta *10
-#	mvmt += speed
-#	translate(Vector3(0,sin(mvmt*0.5),0))
+	var speed = delta *10
+	mvmt += speed
+	base_translation + Vector3(0, sin(mvmt), 0)
 	pass
