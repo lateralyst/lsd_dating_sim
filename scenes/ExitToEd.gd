fc224a366a95ca2681ffff1c2f6be54aeb333586
nnoby <311017aaa@gmail.com>
2020-04-05 23:50:13 -0400

    files SLIGHTLY organized (will be more)

---
+++ scenes/ExitToEd.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+const otherSceneFile = "res://edhell.tscn"
+var otherScene
+
+func _ready():
+	var topNode = get_node("/root").get_child(0).filename;
+	
+	if topNode == otherSceneFile:
+		pass
+	else:
+		otherScene = load(otherSceneFile)
+		pass
+
+func interact():
+	if otherScene == null:
+		print("can't exit to null scene")
+	else:
+		print("exiting to scene " + otherScene.get_name() + "...")
+		get_tree().change_scene_to(otherScene)
